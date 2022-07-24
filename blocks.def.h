305989f99616d6b5473798f482397173173110ca
Albert Canales Ros <albertcanalesros@gmail.com>
2022-07-24 18:51:41 +0200

    Added all basic blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "rofi-bluetooth --status", 10, 0},
+	{"Wifi: ", "w=$(nmcli connection show --active | grep wifi | awk '{print $1}'); [[ -z $w ]] && w=NC; echo $w", 5, 0},
+	{"Vol: ", "echo \"$(pamixer --get-volume)%\"", 5, 0},
+	{"Bat: ", "b=$(acpi -b | awk 'NF{ print $NF }'); [[ -z $b ]] && b=NC; echo $b", 60, 0},
+	{"", "date '+%d/%m (%a) [%R]'",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
