3469d0df5e28c7694e7dc3fe74c831b9cb5e502f
Albert Canales Ros <albertcanalesros@gmail.com>
2022-09-26 18:14:50 +0200

    Small improvements on modules

--- blocks.def.h
+++ blocks.def.h
@@ -2,9 +2,9 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "rofi-bluetooth --status", 10, 0},
-	{"Wifi: ", "w=$(nmcli connection show --active | grep wifi | awk '{print $1}'); [[ -z $w ]] && w=NC; echo $w", 5, 0},
+	{"", "w=$(nmcli connection show --active | grep wifi | awk '{print $1}'); [[ -z $w ]] && echo 睊 || echo 直 $w", 5, 0},
 	{"Vol: ", "echo \"$(pamixer --get-volume)%\"", 5, 0},
-	{"Bat: ", "b=$(acpi -b | awk 'NF{ print $NF }'); [[ -z $b ]] && b=NC; echo $b", 60, 0},
+	{"Bat: ", "b=$(acpi -b | grep -Eo '[0-9]{1,}%'); [[ -z $b ]] && b=NC; echo $b", 60, 0},
 	{"", "date '+%d/%m (%a) [%R]'",					5,		0},
 };
 
